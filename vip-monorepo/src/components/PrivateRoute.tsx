9ecae504c91b3f30f1881fdd0eaaed50ad8f74af
Prover1987 <vered87@mail.ru>
2025-06-22 19:27:10 +0700

    41

--- vip-monorepo/src/components/PrivateRoute.tsx
+++ vip-monorepo/src/components/PrivateRoute.tsx
@@ -1,1 +1,19 @@
- 
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../auth/AuthenticationContext';
+
+const PrivateRoute: React.FC<{ children: React.ReactElement }> = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;

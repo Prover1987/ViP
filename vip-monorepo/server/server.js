93e0e1d2d41d5fc956d3d916211df66e4ea729b9
Prover1987 <vered87@mail.ru>
2025-06-22 15:44:18 +0700

    25

--- vip-monorepo/server/server.js
+++ vip-monorepo/server/server.js
@@ -4,7 +4,23 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+// Настройка CORS
+const allowedOrigins = [
+  'http://localhost:3000', // для локальной разработки
+  'https://vip-1-iwta.onrender.com' // ваш задеплоенный фронтенд
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
